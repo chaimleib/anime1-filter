100f24fcb078d820267d42b78b1e34c6114236e0
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2014-10-06 13:30:34 -0700

    tabs to spaces

--- anime1-filter.js
+++ anime1-filter.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name       Anime1 filter
-// @version    0.1
+// @version    0.2.0a
 // @description  Removes links to objectionable adult content.
 // @match      *.anime1.com/*
-// @copyright  2012+, You
+// @copyright  2012-2014, Chaim-Leib Halbert
 // ==/UserScript==
 
 
@@ -22,66 +22,66 @@ function addJQuery(callback) {
 // the guts of this userscript
 function main() {
     // Lower-case list of terms to filter
-	var censor_list = [
-    	'ecchi',
+    var censor_list = [
+        'ecchi',
         'harem',
         'shoujo ai',
         'shonen ai',
         'shounen ai',
-	]
+    ]
         
     function should_censor(str) {
-    	str = str.toLowerCase();
- 		var censored = '';
+        str = str.toLowerCase();
+        var censored = '';
     
-    	for (var i=0; i<censor_list.length; i++) {
-	        if (censor_list[i] == str) {
-            	console.log('should_censor(\'' + str + '\')');
+        for (var i=0; i<censor_list.length; i++) {
+            if (censor_list[i] == str) {
+                console.log('should_censor(\'' + str + '\')');
                 return true;
             }
-    	}
-    	return false;
-	}
+        }
+        return false;
+    }
 
-	function nodes_to_string(nodes) {
-    	var result = '[';
-    	for (var i=0; i<nodes.length; i++) {
-    		var node = nodes[i];
-			result += node.innerHTML + ', ';
-    	}
-    	result += ']';
-		return result;
-	}    
+    function nodes_to_string(nodes) {
+        var result = '[';
+        for (var i=0; i<nodes.length; i++) {
+            var node = nodes[i];
+            result += node.innerHTML + ', ';
+        }
+        result += ']';
+        return result;
+    }    
 
-	// filters the genre links list
-	function filter_genre_links() {
-		var genre_ary = jQ('.popularity-by-genre > ul a');
-		if (genre_ary.length == 0) {
-        	//console.log('no genres found!');
-	     	return;
-    	}
+    // filters the genre links list
+    function filter_genre_links() {
+        var genre_ary = jQ('.popularity-by-genre > ul a');
+        if (genre_ary.length == 0) {
+            //console.log('no genres found!');
+            return;
+        }
 
-		for (var i=0; i<genre_ary.length; i++) {
-			var a = genre_ary[i];
-        	var li = a.parentNode;
-    	    if (should_censor(a.innerHTML)) {
-            	li.parentNode.removeChild(li);
-        	}
-    	}
-	}
+        for (var i=0; i<genre_ary.length; i++) {
+            var a = genre_ary[i];
+            var li = a.parentNode;
+            if (should_censor(a.innerHTML)) {
+                li.parentNode.removeChild(li);
+            }
+        }
+    }
 
-	// given a billboard node, return whether it should be censored.
-	function should_censor_billboard(bb) {
-    	var genres = jQ(bb).find('div.dgenres a');
+    // given a billboard node, return whether it should be censored.
+    function should_censor_billboard(bb) {
+        var genres = jQ(bb).find('div.dgenres a');
         for (var i=0; i<genres.length; i++) {
             var genre = genres[i].innerHTML;
             if (should_censor(genre)) return true;
         }
         return false;
-	}
+    }
 
-	// filters the list of billboard thumbs
-	function filter_billboards() {
+    // filters the list of billboard thumbs
+    function filter_billboards() {
         var bbs = jQ('div.an-box');
         if (bbs.length == 0) {
             console.log('no bbs found!');
@@ -99,10 +99,10 @@ function main() {
                 bb.parentNode.removeChild(bb);
             }
         }           
-	}
+    }
 
-	filter_genre_links();
-	filter_billboards();
+    filter_genre_links();
+    filter_billboards();
 }
 // load jQuery and execute the main function
 addJQuery(main);
